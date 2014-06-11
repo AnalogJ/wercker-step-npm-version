2fad8bab2c2f28ce95d366a7a0ccd7996aa92dd8
jason <jk17@ualberta.ca>
2014-06-10 20:35:12 -0700

    fixed some issues, update test.

--- run.sh
+++ run.sh
@@ -11,15 +11,15 @@ fi
 
 echo "Step1: get the latest tag in the repo"
 LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo " - latest tag: $LATEST_TAG
+echo " - latest tag: $LATEST_TAG"
 
 echo "Step2: check the diff between the latest tag and the current version."
-if [ git diff $LATEST_TAG --exit-code ];
+if [ git diff $LATEST_TAG --exit-code --quiet ];
 then
- echo " - A difference exists between the current branch () and tag ($LATEST_TAG)"
+ echo " - A difference exists between the current branch [ $WERCKER_GIT_BRANCH ] and tag [ $LATEST_TAG ]"
  echo "Step3: bumping version"
  #bump the version
- npm version $WERCKER_NPM_VERSION_VERSION_TYPE -m "$WERCKER_NPM_VERSION_VERSION_MESSAGE"
+ #npm version $WERCKER_NPM_VERSION_VERSION_TYPE -m "$WERCKER_NPM_VERSION_VERSION_MESSAGE"
 else
  echo " - No change. exiting..."
 fi
