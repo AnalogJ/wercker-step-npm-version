1acf5cb325032fd397569acdb050b2fe47d094b1
jason <jk17@ualberta.ca>
2014-06-11 19:00:04 -0700

    update code.

--- run.sh
+++ run.sh
@@ -15,13 +15,13 @@ echo " - latest tag: $LATEST_TAG"
 
 echo "Step2: check the diff between the latest tag and the current version."
 set +e
-output=$(git diff $LATEST_TAG HEAD --exit-code --quiet);
+output=$(git diff $LATEST_TAG HEAD --exit-code);
 if [ $? -ne 0 ]; then
  echo " - A difference exists between the current branch $WERCKER_GIT_BRANCH and tag $LATEST_TAG"
  echo "Step3: bumping version"
  #bump the version
  echo "$WERCKER_NPM_VERSION_VERSION_TYPE $WERCKER_NPM_VERSION_VERSION_MESSAGE"
- npm version $WERCKER_NPM_VERSION_VERSION_TYPE -m "$WERCKER_NPM_VERSION_VERSION_MESSAGE"
+ #npm version $WERCKER_NPM_VERSION_VERSION_TYPE -m "$WERCKER_NPM_VERSION_VERSION_MESSAGE"
 else
  echo " - No change. exiting..."
 fi
