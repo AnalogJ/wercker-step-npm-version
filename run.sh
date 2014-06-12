58cbd77456fd6b80372d9f2ec7ae6bcba84a9e95
jason <jk17@ualberta.ca>
2014-06-11 17:27:32 -0700

    update code.

--- run.sh
+++ run.sh
@@ -14,8 +14,9 @@ LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo " - latest tag: $LATEST_TAG"
 
 echo "Step2: check the diff between the latest tag and the current version."
-git diff $LATEST_TAG HEAD --exit-code --quiet
-if [ $? -ne 0  ]; then
+RES=$(git diff $LATEST_TAG HEAD --exit-code --quiet)
+echo "test echo"
+if [ RES -ne 0  ]; then
  echo " - A difference exists between the current branch $WERCKER_GIT_BRANCH and tag $LATEST_TAG"
  echo "Step3: bumping version"
  #bump the version
